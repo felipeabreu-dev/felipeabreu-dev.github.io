e2916e39f14cec174b3a31fae4b5afa5679765b6
Felipe Abreu <abreufelipe12@hotmail.com>
2025-08-22 12:48:22 -0300

    Finalização do projeto

--- detona-ralph/src/scripts/engine.js
+++ detona-ralph/src/scripts/engine.js
@@ -6,9 +6,33 @@ const state = {
         score: document.querySelector("#score")
     },
     values:{
-        timerId: null,
-        gameVelocity: 1000
+        gameVelocity: 1000,
+        hitPosition: 0,
+        result: 0,
+        currentTime: 60
+    },
+    actions: {
+        timerId: setInterval(randomSquare, 1000),
+        countDownTimerId: setInterval(countDown, 1000),
     }
+}
+
+function countDown() {
+    state.values.currentTime--;
+    state.view.timeLeft.textContent = state.values.currentTime;
+
+    if(state.values.currentTime <= 0){
+        state.view.timeLeft.textContent = 0;
+        alert(`Gamer Over! O seu resultado foi: ${state.values.result}`);
+        clearInterval(state.actions.countDownTimerId);
+        clearInterval(state.clearInterval.timerId);
+    }
+}
+
+function playSound(audioName) {
+    let audio = new Audio(`./src/audios/${audioName}.m4a`);
+    audio.volume = 0.2;
+    audio.play();
 }
 
 function randomSquare() {
@@ -19,6 +43,7 @@ function randomSquare() {
     let randomNumber = Math.floor(Math.random() * 9);
     let randomSquare = state.view.squares[randomNumber];
     randomSquare.classList.add("enemy");
+    state.values.hitPosition = randomSquare.id;
 }
 
 function moveEnemy() {
@@ -26,11 +51,20 @@ function moveEnemy() {
 }
 
 function addListenerHitBox() {
-    state.view.squares.forEach((square) => {})
+    state.view.squares.forEach((square) => {
+        square.addEventListener("mousedown", () => {
+            if(square.id === state.values.hitPosition) {
+                state.values.result++;
+                state.view.score.textContent = state.values.result;
+                state.values.hitPosition = null;
+                playSound("hit");
+            }
+        })
+    })
 }
 
 function initialize() {
-    moveEnemy();
+    addListenerHitBox();
 }
 
 initialize();
